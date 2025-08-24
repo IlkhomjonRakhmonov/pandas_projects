860cba1945173b1cff59621ad4d3fb15b60cc39f
Rahmonov Ilhomjon <ilhomjonrahmonov1105@gmail.com>
2025-08-24 09:56:12 +0500

    Emojilar qo'shildi

---
+++ emoji.py
@@ -0,0 +1,22 @@
+emoji = {
+    "papka": "\U0001F4C1",           # 📁
+    "hujjat": "\U0001F4C4",          # 📄
+    "yozuv": "\U0001F4DD",           # 📝
+    "grafik": "\U0001F4CA",          # 📊
+    "ok": "\u2705",                  # ✅
+    "xato": "\u274C",                # ❌
+    "ogohlantirish": "\u26A0\uFE0F", # ⚠️
+    "kutish": "\u23F3",              # ⏳
+    "yangilash": "\U0001F501",       # 🔄
+    "qidirish": "\U0001F50D",        # 🔍
+    "qulf": "\U0001F512",            # 🔒
+    "kompyuter": "\U0001F5A5",       # 🖥
+    "saqlash": "\U0001F4BE",         # 💾
+    "fayllar": "\U0001F5C2",         # 🗂
+    "ishga_tushirish": "\U0001F680", # 🚀
+    "sozlamalar": "\U0001F6E0",      # 🛠
+    "tuzatish": "\U0001F527",        # 🔧
+    "paket": "\U0001F4E6",           # 📦
+    "ai": "\U0001F9E0",              # 🧠
+    "sana": "\U0001F4C5"             # 📅
+}
