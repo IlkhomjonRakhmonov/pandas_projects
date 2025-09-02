288cf269384a97777882a2642da73386b87f5521
Rahmonov Ilhomjon <ilhomjonrahmonov1105@gmail.com>
2025-09-02 12:30:30 +0500

    mashqlar

---
+++ Untitled-1.py
@@ -0,0 +1,55 @@
+import pandas as pd
+
+# data = {
+#     'Name': ['Alice', 'Bob', 'Charlie'],
+#     'Age': [25, 30, 35],
+#     'City': ['New York', 'Los Angeles', 'Chicago']
+# }
+
+# df = pd.DataFrame(data)
+
+
+# print(df)
+df= pd.read_csv(r"C:\Users\Abubakir\Downloads\sales_data.csv")
+# print(df)
+
+# df['total_sale'] = df['quantity'] * df['price']
+# total_sale=df['total_sale']
+
+# result=df.groupby('category').agg(
+#                    total_sales=('total_sale', 'sum'),
+#                    max_category=('total_sales', 'max'))
+# print(result)
+
+df['total_sale'] = df['quantity'] * df['price']
+
+
+# 2. Category bo‘yicha jami savdo summasini hisoblash
+result = df.groupby('category').agg(
+    total_sales=('total_sale', 'sum')  # Faqat bitta agg qiling!
+).reset_index()
+
+print(result)
+
+max_cat=result.loc[result['total_sales'].idxmax(), 'category']
+
+max_r=result['total_sales'].max()
+
+max_R =result.loc[result['total_sales'].idxmax(), 'total_sales'] 
+
+print(f"Eng ko'p savdo qilingan kategoriya: {max_R} natija bilan {max_cat} egalladi")
+print()
+print(f"Eng ko'p savdo qilingan kategoriya: {max_r} natija bilan {max_cat} egalladi")
+
+
+
+
+
+
+
+
+
+
+
+
+
